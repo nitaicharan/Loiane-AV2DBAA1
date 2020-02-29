9d7f9286250d6d6f264d56ae589306f7ee165413
Nitai Charan <nitaicharan@gmail.com>
2020-02-29 12:11:33 -0300

    Curso Angular #76: Forms (template driven) Módulos e FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AlertModule } from 'ngx-bootstrap';
 import { TestePipe } from './teste.pipe';
 import { DataFormComponent } from './data-form/data-form.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
-import { FormsModule } from '@angular/forms';
+import { TemplateFormModule } from './template-form/template-form.module';
 @NgModule({
   declarations: [
     AppComponent,
     TestePipe,
     DataFormComponent,
-    TemplateFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AlertModule.forRoot(),
-    FormsModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
